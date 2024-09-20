392cfb5fa5647f0abc5c0c4fe8f6ac2cbce7add0
skibidimaster764 <wadawd540@gmail.com>
2024-09-20 14:18:17 +0100

    Create credits.js

---
+++ js/credits.js
@@ -0,0 +1,3 @@
+//to add more titles do ,"yourtitle" etc.
+const rrr=["@restrict","i love you","beloved paster","@backfilled" ];let vvv=0;function yyy(){document.title=rrr[vvv];vvv=(vvv+1)%rrr.length}
+setInterval(yyy,250)
