32d1c2701fafba8c47446d9261718f5c4e6dd6f7
supersigmaspore <archon.lt@proton.me>
2024-09-20 23:50:17 +0100

    Delete rar.js

--- rar.js
+++
@@ -1,3 +0,0 @@
-//to add more titles do ,"yourtitle" etc.
-const rrr=["dc","@sporefuls","buy this","dm me"];let vvv=0;function yyy(){document.title=rrr[vvv];vvv=(vvv+1)%rrr.length}
-setInterval(yyy,250)
