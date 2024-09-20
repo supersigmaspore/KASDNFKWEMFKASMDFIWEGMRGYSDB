c6aee1812d9cbd84b3e799cd6663a59ec30708f6
supersigmaspore <archon.lt@proton.me>
2024-09-20 01:02:12 +0100

    Update changer.js

--- changer.js
+++ changer.js
@@ -1,3 +1,3 @@
 //to add more titles do ,"yourtitle" etc.
-const rrr=["@restrict made this","i love you" ];let vvv=0;function yyy(){document.title=rrr[vvv];vvv=(vvv+1)%rrr.length}
+const rrr=["spore","runs","you","lol" ];let vvv=0;function yyy(){document.title=rrr[vvv];vvv=(vvv+1)%rrr.length}
 setInterval(yyy,250)
