9dc1240598956b4cbec3b36d3bb5c405c01abee3
supersigmaspore <archon.lt@proton.me>
2024-09-23 22:44:03 +0100

    Create lols.js

---
+++ js/lols.js
@@ -0,0 +1,34 @@
+const TEXTS = [
+    "dm @sporefuls to buy links",
+    "i own abusive.me",
+    "restrict top 1 gambler",
+]
+
+async function sleep(ms) {
+    await new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function doType(text) {
+    typedOut.innerText = ""
+
+    for (let i = 0; i < text.length; i++) {
+        typedOut.innerText = text.slice(0, i + 1)
+        await sleep(100)
+    }
+    await sleep(1000)
+
+    while (typedOut.innerText.length !== 0) {
+        typedOut.innerText = typedOut.innerText.slice(0, typedOut.innerText.length - 1)
+        await sleep(50)
+    }
+}
+
+async function typeloop() {
+    while (true) {
+        for (let i = 0; i < TEXTS.length; i++) {
+            await doType(TEXTS[i])
+        }
+    }
+}
+
+typeloop()
