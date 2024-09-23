6bcd35918fbbc2e94569b4372ec18199b6aea04b
supersigmaspore <archon.lt@proton.me>
2024-09-23 21:35:28 +0100

    Create stupid.cshtml.cs

---
+++ stupid.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace YourAppNamespace.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Add any logic you need to run when the page is accessed.
+        }
+    }
+}
