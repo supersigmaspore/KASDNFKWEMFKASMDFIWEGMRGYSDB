c2877ef0a301e25c21e77a9a2401dc1c115bcab8
supersigmaspore <archon.lt@proton.me>
2024-10-05 21:58:26 +0100

    Update rar.js

--- js/rar.js
+++ js/rar.js
@@ -1,3 +1,3 @@
 //to add more titles do ,"yourtitle" etc.
-const rrr=["dc","@sporefuls","buy this","dm me"];let vvv=0;function yyy(){document.title=rrr[vvv];vvv=(vvv+1)%rrr.length}
+const rrr=["dc","@stupids","buy this","dm me"];let vvv=0;function yyy(){document.title=rrr[vvv];vvv=(vvv+1)%rrr.length}
 setInterval(yyy,250)
