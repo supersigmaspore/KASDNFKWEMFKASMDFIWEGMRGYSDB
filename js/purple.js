24c6989426199540389dad0fc42b4549d2033dc5
supersigmaspore <archon.lt@proton.me>
2024-11-07 21:25:04 +0000

    Update purple.js

--- js/purple.js
+++ js/purple.js
@@ -173,6 +173,6 @@ function createDiv(height, width) {
 }
 
 function newColour() {
-	const colors = ['#000000', '#000000'];
+	const colors = ['white', 'white'];
 	return colors[Math.floor(Math.random() * colors.length)];
 }
