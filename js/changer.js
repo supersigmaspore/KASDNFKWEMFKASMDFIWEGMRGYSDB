2115b99a9d0dc6db3de98ff45f88e12fa2a3c8ae
supersigmaspore <archon.lt@proton.me>
2025-03-02 16:00:23 +0000

    Update changer.js

--- js/changer.js
+++ js/changer.js
@@ -1,3 +1,3 @@
 //to add more titles do ,"yourtitle" etc.
-const rrr=["buy this","untouched","ur slow","LOL" ];let vvv=0;function yyy(){document.title=rrr[vvv];vvv=(vvv+1)%rrr.length}
+const rrr=["cut","slit","ur slow","LOL","fuck you" ];let vvv=0;function yyy(){document.title=rrr[vvv];vvv=(vvv+1)%rrr.length}
 setInterval(yyy,250)
