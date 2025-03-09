53c238d412a1be4137074e41447d914da5f5cff3
skibidimaster764 <restrict@gmx.com>
2025-03-09 13:43:24 +0000

    Delete js directory

--- js/credits.js
+++
@@ -1,3 +0,0 @@
-//to add more titles do ,"yourtitle" etc.
-const rrr=["@restrict","i love you","beloved paster","@backfilled" ];let vvv=0;function yyy(){document.title=rrr[vvv];vvv=(vvv+1)%rrr.length}
-setInterval(yyy,250)
--- js/pagecopy.js
+++
@@ -1,1 +0,0 @@
-let copydisc=document.getElementById("copyclipboard");let disc=document.getElementById("disc");disc.addEventListener("click",function(){navigator.clipboard.writeText(copydisc.textContent);copydisc.textContent="Copied"})
